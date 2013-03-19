3bfe792906a40c3f4648fca1b3545c1e84787556
Capripot <capripot@gmail.com>
2013-03-19 16:11:57 -0400

    working exemple

---
+++ data.php
@@ -0,0 +1,134 @@
+<?php
+
+$data = array();
+$data[] = array("option_id" => "1","blog_id" => "0","option_name" => "siteurl","option_value" => "http://www.omnisdata.com","autoload" => "yes");
+$data[] = array("option_id" => "2","blog_id" => "0","option_name" => "blogname","option_value" => "Omnisdata","autoload" => "yes");
+$data[] = array("option_id" => "3","blog_id" => "0","option_name" => "blogdescription","option_value" => "Omnisdata","autoload" => "yes");
+$data[] = array("option_id" => "4","blog_id" => "0","option_name" => "users_can_register","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "5","blog_id" => "0","option_name" => "admin_email","option_value" => "info@omnisdata.com","autoload" => "yes");
+$data[] = array("option_id" => "6","blog_id" => "0","option_name" => "start_of_week","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "7","blog_id" => "0","option_name" => "use_balanceTags","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "8","blog_id" => "0","option_name" => "use_smilies","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "9","blog_id" => "0","option_name" => "require_name_email","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "10","blog_id" => "0","option_name" => "comments_notify","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "11","blog_id" => "0","option_name" => "posts_per_rss","option_value" => "10","autoload" => "yes");
+$data[] = array("option_id" => "12","blog_id" => "0","option_name" => "posts_per_atom","option_value" => "10","autoload" => "yes");
+$data[] = array("option_id" => "13","blog_id" => "0","option_name" => "rss_use_excerpt","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "14","blog_id" => "0","option_name" => "mailserver_url","option_value" => "mail.example.com","autoload" => "yes");
+$data[] = array("option_id" => "15","blog_id" => "0","option_name" => "mailserver_login","option_value" => "login@example.com","autoload" => "yes");
+$data[] = array("option_id" => "16","blog_id" => "0","option_name" => "mailserver_pass","option_value" => "password","autoload" => "yes");
+$data[] = array("option_id" => "17","blog_id" => "0","option_name" => "mailserver_port","option_value" => "110","autoload" => "yes");
+$data[] = array("option_id" => "18","blog_id" => "0","option_name" => "default_category","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "19","blog_id" => "0","option_name" => "default_comment_status","option_value" => "closed","autoload" => "yes");
+$data[] = array("option_id" => "20","blog_id" => "0","option_name" => "default_ping_status","option_value" => "open","autoload" => "yes");
+$data[] = array("option_id" => "21","blog_id" => "0","option_name" => "default_pingback_flag","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "22","blog_id" => "0","option_name" => "default_post_edit_rows","option_value" => "10","autoload" => "yes");
+$data[] = array("option_id" => "23","blog_id" => "0","option_name" => "posts_per_page","option_value" => "10","autoload" => "yes");
+$data[] = array("option_id" => "24","blog_id" => "0","option_name" => "what_to_show","option_value" => "posts","autoload" => "yes");
+$data[] = array("option_id" => "25","blog_id" => "0","option_name" => "date_format","option_value" => "F j, Y","autoload" => "yes");
+$data[] = array("option_id" => "26","blog_id" => "0","option_name" => "time_format","option_value" => "g:i a","autoload" => "yes");
+$data[] = array("option_id" => "27","blog_id" => "0","option_name" => "links_updated_date_format","option_value" => "F j, Y g:i a","autoload" => "yes");
+$data[] = array("option_id" => "28","blog_id" => "0","option_name" => "links_recently_updated_prepend","option_value" => "<em>","autoload" => "yes");
+$data[] = array("option_id" => "29","blog_id" => "0","option_name" => "links_recently_updated_append","option_value" => "<\/em>","autoload" => "yes");
+$data[] = array("option_id" => "30","blog_id" => "0","option_name" => "links_recently_updated_time","option_value" => "120","autoload" => "yes");
+$data[] = array("option_id" => "31","blog_id" => "0","option_name" => "comment_moderation","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "32","blog_id" => "0","option_name" => "moderation_notify","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "33","blog_id" => "0","option_name" => "permalink_structure","option_value" => "\/%category%\/%postname%\/","autoload" => "yes");
+$data[] = array("option_id" => "34","blog_id" => "0","option_name" => "gzipcompression","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "35","blog_id" => "0","option_name" => "hack_file","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "36","blog_id" => "0","option_name" => "blog_charset","option_value" => "UTF-8","autoload" => "yes");
+$data[] = array("option_id" => "37","blog_id" => "0","option_name" => "moderation_keys","option_value" => "","autoload" => "no");
+$data[] = array("option_id" => "38","blog_id" => "0","option_name" => "active_plugins","option_value" => "1,2,3","autoload" => "yes");
+$data[] = array("option_id" => "310","blog_id" => "0","option_name" => "recently_activated","option_value" => "a:0:{}","autoload" => "yes");
+$data[] = array("option_id" => "39","blog_id" => "0","option_name" => "home","option_value" => "http:\/\/www.omnisdata.com","autoload" => "yes");
+$data[] = array("option_id" => "40","blog_id" => "0","option_name" => "category_base","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "41","blog_id" => "0","option_name" => "ping_sites","option_value" => "http:\/\/rpc.pingomatic.com\/","autoload" => "yes");
+$data[] = array("option_id" => "42","blog_id" => "0","option_name" => "advanced_edit","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "43","blog_id" => "0","option_name" => "comment_max_links","option_value" => "2","autoload" => "yes");
+$data[] = array("option_id" => "44","blog_id" => "0","option_name" => "gmt_offset","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "45","blog_id" => "0","option_name" => "default_email_category","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "46","blog_id" => "0","option_name" => "recently_edited","option_value" => "23","autoload" => "no");
+$data[] = array("option_id" => "47","blog_id" => "0","option_name" => "use_linksupdate","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "48","blog_id" => "0","option_name" => "template","option_value" => "4u","autoload" => "yes");
+$data[] = array("option_id" => "49","blog_id" => "0","option_name" => "stylesheet","option_value" => "4u","autoload" => "yes");
+$data[] = array("option_id" => "50","blog_id" => "0","option_name" => "comment_whitelist","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "52","blog_id" => "0","option_name" => "blacklist_keys","option_value" => "","autoload" => "no");
+$data[] = array("option_id" => "53","blog_id" => "0","option_name" => "comment_registration","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "54","blog_id" => "0","option_name" => "rss_language","option_value" => "en","autoload" => "yes");
+$data[] = array("option_id" => "55","blog_id" => "0","option_name" => "html_type","option_value" => "text\/html","autoload" => "yes");
+$data[] = array("option_id" => "56","blog_id" => "0","option_name" => "use_trackback","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "57","blog_id" => "0","option_name" => "default_role","option_value" => "subscriber","autoload" => "yes");
+$data[] = array("option_id" => "58","blog_id" => "0","option_name" => "db_version","option_value" => "12329","autoload" => "yes");
+$data[] = array("option_id" => "59","blog_id" => "0","option_name" => "uploads_use_yearmonth_folders","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "60","blog_id" => "0","option_name" => "upload_path","option_value" => "\/var\/www\/vhosts\/omnisdata.com\/httpdocs\/wp-content\/uploads","autoload" => "yes");
+$data[] = array("option_id" => "62","blog_id" => "0","option_name" => "secret","option_value" => "1OsM%j98f!5I)bXR17x7xsDZK2gR)(r%zy@io6NyU6i$7cKe!I&VXkYJhc7@RxTd","autoload" => "yes");
+$data[] = array("option_id" => "63","blog_id" => "0","option_name" => "blog_public","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "64","blog_id" => "0","option_name" => "default_link_category","option_value" => "2","autoload" => "yes");
+$data[] = array("option_id" => "65","blog_id" => "0","option_name" => "show_on_front","option_value" => "posts","autoload" => "yes");
+$data[] = array("option_id" => "66","blog_id" => "0","option_name" => "tag_base","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "67","blog_id" => "0","option_name" => "show_avatars","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "68","blog_id" => "0","option_name" => "avatar_rating","option_value" => "G","autoload" => "yes");
+$data[] = array("option_id" => "69","blog_id" => "0","option_name" => "upload_url_path","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "70","blog_id" => "0","option_name" => "thumbnail_size_w","option_value" => "150","autoload" => "yes");
+$data[] = array("option_id" => "71","blog_id" => "0","option_name" => "thumbnail_size_h","option_value" => "150","autoload" => "yes");
+$data[] = array("option_id" => "72","blog_id" => "0","option_name" => "thumbnail_crop","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "73","blog_id" => "0","option_name" => "medium_size_w","option_value" => "300","autoload" => "yes");
+$data[] = array("option_id" => "74","blog_id" => "0","option_name" => "medium_size_h","option_value" => "300","autoload" => "yes");
+$data[] = array("option_id" => "75","blog_id" => "0","option_name" => "avatar_default","option_value" => "mystery","autoload" => "yes");
+$data[] = array("option_id" => "76","blog_id" => "0","option_name" => "enable_app","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "77","blog_id" => "0","option_name" => "enable_xmlrpc","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "78","blog_id" => "0","option_name" => "large_size_w","option_value" => "1024","autoload" => "yes");
+$data[] = array("option_id" => "79","blog_id" => "0","option_name" => "large_size_h","option_value" => "1024","autoload" => "yes");
+$data[] = array("option_id" => "80","blog_id" => "0","option_name" => "image_default_link_type","option_value" => "file","autoload" => "yes");
+$data[] = array("option_id" => "81","blog_id" => "0","option_name" => "image_default_size","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "82","blog_id" => "0","option_name" => "image_default_align","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "83","blog_id" => "0","option_name" => "close_comments_for_old_posts","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "84","blog_id" => "0","option_name" => "close_comments_days_old","option_value" => "14","autoload" => "yes");
+$data[] = array("option_id" => "85","blog_id" => "0","option_name" => "thread_comments","option_value" => "","autoload" => "yes");
+$data[] = array("option_id" => "86","blog_id" => "0","option_name" => "thread_comments_depth","option_value" => "5","autoload" => "yes");
+$data[] = array("option_id" => "87","blog_id" => "0","option_name" => "page_comments","option_value" => "1","autoload" => "yes");
+$data[] = array("option_id" => "88","blog_id" => "0","option_name" => "comments_per_page","option_value" => "50","autoload" => "yes");
+$data[] = array("option_id" => "89","blog_id" => "0","option_name" => "default_comments_page","option_value" => "newest","autoload" => "yes");
+$data[] = array("option_id" => "90","blog_id" => "0","option_name" => "comment_order","option_value" => "asc","autoload" => "yes");
+$data[] = array("option_id" => "91","blog_id" => "0","option_name" => "use_ssl","option_value" => "0","autoload" => "yes");
+$data[] = array("option_id" => "92","blog_id" => "0","option_name" => "sticky_posts","option_value" => "a:0:{}","autoload" => "yes");
+$data[] = array("option_id" => "93","blog_id" => "0","option_name" => "widget_categories","option_value" => "a:2:{i:2;a:0:{}s:12:\"_multiwidget\";i:1;}","autoload" => "yes");
+$data[] = array("option_id" => "94","blog_id" => "0","option_name" => "widget_text","option_value" => "a:2:{i:2;a:0:{}s:12:\"_multiwidget\";i:1;}","autoload" => "yes");
+$data[] = array("option_id" => "95","blog_id" => "0","option_name" => "widget_rss","option_value" => "a:2:{i:2;a:0:{}s:12:\"_multiwidget\";i:1;}","autoload" => "yes");
+$data[] = array("option_id" => "97","blog_id" => "0","option_name" => "dismissed_update_core","option_value" => "a:0:{}","autoload" => "yes");
+$data[] = array("option_id" => "100","blog_id" => "0","option_name" => "auth_salt","option_value" => "d$*gp6edWtbn","autoload" => "yes");
+$data[] = array("option_id" => "104","blog_id" => "0","option_name" => "logged_in_salt","option_value" => "y2#Pp#rFW&Vp","autoload" => "yes");
+$data[] = array("option_id" => "106","blog_id" => "0","option_name" => "nonce_salt","option_value" => "1FVmeoPugbxj","autoload" => "yes");
+
+
+$pagination = false;
+if ( isset($_REQUEST["page"]) )
+{
+	$pagination = true;
+	
+	$page = intval($_REQUEST["page"]);
+	$perpage = intval($_REQUEST["perpage"]);
+}
+
+// this variables Omnigrid will send only if serverSort option is true
+$sorton = $_REQUEST["sorton"];
+$sortby = $_REQUEST["sortby"];
+
+
+if($pagination){
+	$n = ( $page -1 ) * $perpage;
+	$nmax = $n+$perpage;
+	$ret = array();
+
+	for($i = $n; $i < $nmax && $i < sizeof($data); $i++){
+		array_push($ret, $data[$i]);
+	}
+}
+else
+	$ret = $data;
+
+$ret = array("page"=>$page, "total"=>sizeof($data), "data"=>$ret);
+
+echo json_encode($ret);
+
+?>
